e1b0dec9eb8e2feaa2488fe75a5b25e72944ab98
javierjose25 <javierjose2555@gmail.com>
2020-07-22 19:35:25 -0500

    pc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" > <h1>PROYECTO PPI: PSITOPÍA NET</h1> <div class="wrapper">
+  
+   <div class="item2">Nombre</div>
+   <div class="item2">Apellidos</div>
+   <div class="item2">Correo</div>
+   <div class="item2">Celular</div>
+   <div class="item">Brayan</div>
+   <div class="item">Espinosa Tafur</div>
+   <div class="item">brayanespinosa258@gmail.com</div>
+   <div class="item">3233536841</div>
+   <div class="item">Javier José</div>
+   <div class="item">González Pacheco</div>
+   <div class="item">javierjose2555@gmail.com</div>
+   <div class="item">3158946859</div>
+   <div class="item">Juliana Andrea</div>
+   <div class="item">Ibarguen Mejía</div>
+   <div class="item">mejia.1d2002@gmail.com</div>
+   <div class="item">3176771016</div>
+   <div class="item">Andrés Felipe</div>
+   <div class="item">Bejarano López</div>
+   <div class="item">andres1231991@gmail.com</div>
+   <div class="item">3137873037</div>
+ </div>
+
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+</div>;
 }
+
 
 export default App;
